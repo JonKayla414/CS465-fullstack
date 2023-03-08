97c454904f4807ac6562a64e7409550f732112ac
pointerjonkayla <jon-kayla.pointer@snhu.edu>
2023-03-07 20:54:07 -0600

    Module 2 MVC

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const ctrlMain = require('../controllers/main');
+router.get('/', ctrlMain.index);
 
 module.exports = router;
