224c7467c64b131067969e37f23ec3e8a5d5da6f
pointerjonkayla <jon-kayla.pointer@snhu.edu>
2023-03-16 08:02:27 -0500

    MOD 3
    Json files added and rendered. Background and logo changed.

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+var main = JSON.parse(fs.readFileSync('./data/main.json',
+'utf8'));
+
 /* GET Homepage  */
 const index = (req, res) => {
-    res.render('index', {title:'Travlr Getaways '});
+    res.render('index', {title:'Travlr Getaways', main});
 };
 module.exports ={
     index
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,12 @@
+
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json',
+'utf8'));
+
+
+/* Get Travel view */
 const travel = (req, res) => {
-    res.render('travel', {title:'Travlr Getaways '});
+    res.render('travel', {title:'Travlr Getaways', trips});
 };
 module.exports = {
     travel
